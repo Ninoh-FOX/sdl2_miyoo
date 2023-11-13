5d6b9f97bb75cd24c694ad1c1ae4118b32a0e7f7
Ninoh-FOX <ninohfox@gmail.com>
2023-11-13 15:25:58 +0100

    enable sensor and oss, set toolchain

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export MOD=mmiyoo
-export CROSS=/opt/mmiyoo/bin/arm-linux-gnueabihf-
+export CROSS=/opt/miyoomini-toolchain/bin/arm-linux-gnueabihf-
 export CC=${CROSS}gcc
 export AR=${CROSS}ar
 export AS=${CROSS}as
@@ -11,7 +11,7 @@ export HOST=arm-linux
 echo "Building SDL2 for Miyoo Mini / Miyoo Mini Plus"
 if [ ! -z "$1" ] && [ "$1" == "config" ]; then
     ./autogen.sh
-    ./configure --disable-joystick-virtual --disable-sensor --disable-power --disable-alsa --disable-diskaudio --disable-video-x11 --disable-video-wayland --disable-video-kmsdrm --disable-video-vulkan --disable-dbus --disable-ime --disable-fcitx --disable-hidapi --disable-pulseaudio --disable-sndio --disable-libudev --disable-jack --disable-video-opengl --disable-video-opengles --disable-video-opengles2 --disable-oss --disable-dummyaudio --disable-video-dummy --host=${HOST}
+    ./configure --disable-joystick-virtual --enable-sensor --disable-power --disable-alsa --disable-diskaudio --disable-video-x11 --disable-video-wayland --disable-video-kmsdrm --disable-video-vulkan --disable-dbus --disable-ime --disable-fcitx --disable-hidapi --disable-pulseaudio --disable-sndio --disable-libudev --disable-jack --disable-video-opengl --disable-video-opengles --disable-video-opengles2 --enable-oss --disable-dummyaudio --disable-video-dummy --host=${HOST} --prefix=/opt/miyoomini-toolchain/arm-linux-gnueabihf/sysroot/usr
 fi
 
 make -j4 V=99
